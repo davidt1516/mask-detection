647eb1f52a7d9dbc7dd281b5fb8d5cba0597499b
davidt1516 <48577848+davidt1516@users.noreply.github.com>
2020-05-21 11:26:43 -0500

    Update main.py
    Los comentarios!!!!!

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-video = cv.VideoCapture(0)
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml') # Carga de un filtro de cascada específico para detectar rostros
+video = cv.VideoCapture(0) # Configuración para captura de video de cámara 
 
 while video.isOpened():
-    ret, frame = video.read()
+    ret, frame = video.read() #Lectura de cámara
     if ret:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #Conversión a escala de grises
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4) #Aplicar filtro, determinando los espacios de pixeles que contienen la cara
 
         for (x, y, w, h) in faces:
-            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2) #Dibujar cuadrado en los rostros detectados
 
         cv.imshow('Detector de rostros', frame)
-        if cv.waitKey(25) & 0xFF == ord('q'):
-            quit()
+        if cv.waitKey(25) & 0xFF == ord('q'): #A la espera de pulsar la tecla de finalización
+            #quit() <----------- Esto no tiene función práctica en el código, da error porque la función no está implementada -------------->
             cv.destroyAllWindows()
             break
 
-video.release()
+video.release() #Apagado de la cámara
